fdd0864c0e53392c968423011eda84feac51fe03
RohitGusai <rohitgusai235016@gmail.com>
2024-07-22 23:38:52 +0530

    i am commiting

--- app.js
+++ app.js
@@ -8,3 +8,79 @@
 // shoba is finding a job
 
 // Joker always laugh in front of his auidence but at back stage he is atually sad
+
+let output = document.querySelectorAll(".box");
+let i=1;
+for(input of output)
+{
+    input.innerText = `The Unique value is ${i}`;
+    i++;
+}
+
+let query = document.querySelector('div');
+
+let values = query.getAttribute('value');
+console.log(values);
+
+let access = document.querySelector('p');
+access.classList.add('content2');
+console.log(access.outerHTML);
+
+// function btn()
+// {
+//     console.log('rohit');
+// }
+
+let button = document.querySelector('#btn1');
+let currmode = "light";
+button.addEventListener('click',
+    ()=>
+    {
+        if(currmode == "light") 
+            {
+                currmode = "black";
+                document.querySelector('body').classList.add('change1');
+                document.querySelector('body').classList.remove('change');
+                
+            }
+            else
+            {
+                document.querySelector('body').classList.add('change');
+                document.querySelector('body').classList.remove('change1');
+                currmode = "light";
+            }
+            console.log(currmode);
+        
+    }
+);
+    
+
+
+
+
+
+
+
+let div = document.querySelectorAll('.box');
+
+    div.forEach((e)=>{  
+        e.onmouseover = () =>
+        {
+            console.log("you are inside the div");
+        };
+        e.onmouseout = () =>
+        {
+            console.log("You are outside of the div");
+        };
+});
+        
+    
+    
+
+
+// div.onmouseout = () =>
+// {
+//     console.log("You are outside of the div");
+// }
+
+
